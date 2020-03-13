5295da90b9f566ed105c6c24eebec575713991e6
= <fabioandres31@gmail.com>
2020-03-13 09:14:48 -0500

    Configuraciones de rutas y manejo de logs

--- services/implement/centralexternaservices.js
+++ services/implement/centralexternaservices.js
@@ -1,11 +1,10 @@
-const query = require('../properties-reader/properties');
 const graphql = require('../graphql-request/graphql-request');
 const config = require('../../config/config');
 const logger = require('winston');
 
 let consultaCentralExterna = async (clavePrimaria) => {
     try {
-        let queryRepo = await query.obtenerQueryGraphql(config.get('queryce'));
+        let queryRepo = config.get('queryce');
         let data = await graphql.sendQuery(config.get('endpoint'), queryRepo, {clavePrimaria}/*, graphQLHeaders**/);
         const resString = JSON.stringify(data).split('\\"').join('');
         return JSON.parse(resString);
